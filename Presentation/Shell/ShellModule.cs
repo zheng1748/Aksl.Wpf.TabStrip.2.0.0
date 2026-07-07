a3e67e1cd41791dfa1444982ef8082ab5a790e3a
zheng1748 <zheng1748@outlook.com>
2026-07-07 20:40:59 +0800

    UnityContainer

--- Presentation/Shell/ShellModule.cs
+++ Presentation/Shell/ShellModule.cs
@@ -13,14 +13,12 @@ namespace Aksl.Modules.Shell
     {
         #region Members
         private readonly IUnityContainer _container;
-        private readonly IRegionManager _regionManager;
         #endregion
 
         #region Constructors
-        public ShellModule(IUnityContainer container, IRegionManager regionManager)
+        public ShellModule(IUnityContainer container)
         {
             _container = container;
-            _regionManager = regionManager;
         }
         #endregion
 
@@ -31,14 +29,6 @@ namespace Aksl.Modules.Shell
 
         public void OnInitialized(IContainerProvider containerProvider)
         {
-
-        // _regionManager.RequestNavigate(RegionNames.ShellContentRegion, nameof(Aksl.Modules.HamburgerMenuSideBar.Views.HamburgerMenuSideBarHubView));
-      //  _regionManager.RequestNavigate(RegionNames.ShellContentRegion, nameof(HamburgerMenuNavigationSideBar.Views.HamburgerMenuNavigationSideBarHubView));
-          // _regionManager.RequestNavigate(RegionNames.ShellContentRegion, nameof(Aksl.Modules.HamburgerMenuTreeSideBar.Views.HamburgerMenuTreeSideBarHubView));
-
-          // _regionManager.RequestNavigate(RegionNames.ShellContentRegion, nameof(Aksl.Modules.HamburgerMenuPopupSideBar.Views.HamburgerMenPopupSideBarHubView));
-
-          // _regionManager.RequestNavigate(RegionNames.ShellLoginRegion, nameof(LoginStatusView));
         }
         #endregion
     }
